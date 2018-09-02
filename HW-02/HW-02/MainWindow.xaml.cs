1cda405ddb259e477e2ecf3bc1ab0be1e336645d
Katelyn Peterson <00154664@badgermail.snow.edu>
2018-09-01 18:28:27 -0600

    Add functionality to buttons to change colors and image

--- HW-02/HW-02/MainWindow.xaml.cs
+++ HW-02/HW-02/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
 
 namespace HW_02
 {
@@ -10,26 +14,69 @@ namespace HW_02
     public partial class MainWindow : Window
     {
         int listCount = 0;
+        int click = 0;
+        int click2 = 0;
+        List<String> pictureArray = new List<String>();
+        Uri resourceUri = new Uri("Images/mountain_lake.jpeg", UriKind.Relative);
 
         public MainWindow()
         {
             InitializeComponent();
+            
+            pictureArray.Add("/Images/lilies.jpeg");
+            pictureArray.Add("/Images/swans_on_lake.jpeg");
+            pictureArray.Add("/Images/tree_by_water.jpeg");
+            pictureArray.Add("/Images/wave.jpeg");
+            pictureArray.Add("/Images/tree_lined_lane.jpeg");
+            pictureArray.Add("/Images/tree_in_field.jpeg");
 
-            userName.Text = System.Security.Principal.WindowsIdentity.GetCurrent().Name;           
-        }
+            userName.Text = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            
+            StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
 
-        public Image GetNextImage(int next)
-        {            
-            List<Image> imgArray = new List<Image>();
-            //imgArray.Add(Image.FromFile("c/Users/katel/source/repos/GUI-HW02/pics/"));
-            return imgArray[next];
+            BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+            var brush = new ImageBrush();
+            brush.ImageSource = temp;
+
+            picture.Background = brush;
         }
 
         private void btnClick_getNewPicture(object sender, RoutedEventArgs e)
         {
+            if (listCount < pictureArray.Count)
+            {
+                resourceUri = new Uri(pictureArray[listCount], UriKind.Relative);
+                StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
 
-            listCount += 1;
-            GetNextImage(listCount);
+                BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                var brush = new ImageBrush();
+                brush.ImageSource = temp;
+
+                picture.Background = brush;
+                listCount += 1;
+            }
+            else
+            listCount = 0;
+        }
+
+        private void btnClick_changeColor(object sender, RoutedEventArgs e)
+        {
+            myApp.Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Blue");
+            click += 1;
+            if(click % 2 == 0)
+                myApp.Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("white");
+        }
+
+        private void btnClick_changeFont(object sender, RoutedEventArgs e)
+        {
+            title.Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Orange");
+            welcome.Text = "Howdy";
+            click2 += 1;
+            if (click2 % 2 == 0)
+            {
+                title.Background = (Brush)System.ComponentModel.TypeDescriptor.GetConverter(typeof(Brush)).ConvertFromInvariantString("Purple");
+                welcome.Text = "Welcome";
+            }
         }
     }
 }
